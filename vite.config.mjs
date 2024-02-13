8d8eb875fbdb1c280f9e0d1dd27082b28f238aa1
Dave Naugler <davenaugler@gmail.com>
2024-02-12 21:56:00 -0700

    ✅ added server path to vite.config.mjs file

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@
 export default {
 	server: {
 		host: '0.0.0.0',
-		port: 5173, // Change the port if necessary
+		port: 3000, // Change the port if necessary
 	},
 }
 import {defineConfig} from 'vite'
